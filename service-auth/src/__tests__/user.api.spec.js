9039cdb86aa1637cff7539713dbb1cda5dffac49
vitalii boiko <vitb@ciklum.com>
2019-12-19 13:25:42 +0200

    SOLIP-3419: + user api tests

---
+++ service-auth/src/__tests__/user.api.spec.js
@@ -0,0 +1,60 @@
+import request from 'supertest';
+import { app } from '../server';
+import { connect } from '../utils/db';
+import { User } from '../models/user.model';
+import faker from 'faker';
+import { generateToken } from '../utils/auth';
+
+const testUser = {
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+};
+let server;
+let db;
+
+describe('User API', () => {
+
+  beforeEach(async (done) => {
+    db = await connect();
+    await User.deleteMany({});
+    server = app.listen(4000, () => {
+      global.agent = request.agent(server);
+      done();
+    });
+  });
+
+  afterEach(async () => {
+    await server.close();
+    await db.disconnect();
+  });
+
+  describe('/GET /api/me', () => {
+
+    it('should return user info', async () => {
+      const user = await User.create(testUser);
+      const token = generateToken(user.id);
+      const serializedUser = user.toObject();
+      const { body } = await request(app)
+        .get('/api/me')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+
+      expect(JSON.stringify(body)).toEqual(JSON.stringify(serializedUser));
+    });
+
+    it('should return 401 Unauthorized error when Auth header is not provided', async () => {
+      await request(app)
+        .get('/api/me')
+        .expect(401);
+    });
+
+    it('should return 401 Unauthorized error when token is invalid', async () => {
+      await request(app)
+        .get('/api/me')
+        .set('Authorization', `Bearer ${faker.random.uuid()}`)
+        .expect(401);
+    });
+  });
+});
