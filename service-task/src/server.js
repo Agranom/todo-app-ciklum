e1987d90e6e57fdb9a7c7ea7098385c702da4e64
vitalii boiko <vitb@ciklum.com>
2019-12-25 19:02:55 +0200

    SOLIP-3422: + swagger open api 3.0

--- service-task/src/server.js
+++ service-task/src/server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import morgan from 'morgan';
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import config from './config';
 import { connect } from './utils/db';
 import taskRouter from './routes/task.router';
 import { validateTokenAndGetUser } from './utils/auth';
+import swaggerDoc from '../doc/swagger';
 
 export const app = express();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use('/api/task', validateTokenAndGetUser, taskRouter);
 
