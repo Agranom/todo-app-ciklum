965c8002804f47ed379cc2d253cac1560954b9f3
vitalii boiko <vitb@ciklum.com>
2020-01-06 19:22:58 +0200

    SOLIP-3425: + Dockerrun.aws.json

--- service-auth/src/config/dev.js
+++ service-auth/src/config/dev.js
@@ -1,3 +1,3 @@
 export const config = {
-  dbUrl: 'mongodb://localhost:27017/users'
+  dbUrl: process.env.DB_URL
 };
--- service-auth/src/config/prod.js
+++ service-auth/src/config/prod.js
@@ -1,3 +1,3 @@
 export const config = {
-  dbUrl: 'mongodb://database/users'
+  dbUrl: process.env.DB_URL
 };
