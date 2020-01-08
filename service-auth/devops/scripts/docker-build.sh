20318cc03c14b0ee7ece7b3b70d9f517e41ec46a
vitalii boiko <vitb@ciklum.com>
2020-01-08 13:34:42 +0200

    SOLIP-3425: test

--- service-auth/devops/scripts/docker-build.sh
+++ service-auth/devops/scripts/docker-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker build -f "./devops/docker/Dockerfile" \
-      --build-arg DB_URL=${SVC_AUTH_DB_URL} \
+      --build-arg SVC_AUTH_DB_URL=${SVC_AUTH_DB_URL} \
       --build-arg INTERNAL_PASSWORD=${INTERNAL_PASSWORD} \
       -t agranom/service-auth .
